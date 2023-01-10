dd99437415e71da9ccfebc4045fc2e8dd4095156
wk989898 <wangkun989898@gmail.com>
2023-01-10 17:06:37 +0000

    feat:add valid

--- train.py
+++ train.py
@@ -8,34 +8,49 @@ from data import ARESdataset, collate_fn
 
 def main(args):
     print(args)
-    dataset=ARESdataset(args.dir)
-    # dataloader=torch.utils.data.DataLoader(dataset,batch_size=1,shuffle=True)
-    dataloader=torch.utils.data.DataLoader(dataset,batch_size=args.batch_size,shuffle=True,collate_fn=collate_fn)
+    train_dataset=ARESdataset(args.train_dir)
+    valid_dataset=ARESdataset(args.valid_dir)
+    # train_dataloader=torch.utils.data.DataLoader(train_dataset,batch_size=1,shuffle=True)
+    # valid_dataloader=torch.utils.data.DataLoader(valid_dataset,batch_size=1,shuffle=True)
+    train_dataloader=torch.utils.data.DataLoader(train_dataset,batch_size=args.batch_size,shuffle=True,collate_fn=collate_fn)
+    valid_dataloader=torch.utils.data.DataLoader(valid_dataset,batch_size=args.batch_size,shuffle=True,collate_fn=collate_fn)
     net = Net(device=args.device)
     optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
     loss_fn = torch.nn.HuberLoss()
     if args.checkpoint is not None:
         net.load_state_dict(torch.load(args.checkpoint))
-    net.train()
     for epoch in range(args.epochs):
-        avgloss = 0
-        for i,batch in enumerate(dataloader):
+        train_loss = 0
+        net.train()
+        for i,batch in enumerate(train_dataloader):
             V,atoms_info,rms,atoms_lens = (to_device(x,args.device) for x in batch)
             out = net(V, atoms_info, atoms_lens)
             loss = loss_fn(out, rms)
             loss.backward()
-            avgloss += loss.item()
+            train_loss += loss.item()
             if (i+1) % args.accumulation_steps == 0 or (i+1) == len(dataloader):
                 optimizer.step()
                 optimizer.zero_grad()
-        print(f'epcho:{epoch} loss:{avgloss/len(dataset)}')
+        print(f'epcho:{epoch} train loss:{train_loss/len(train_dataset)}')
+
+        val_loss = 0
+        net.eval()
+        with torch.no_grad():
+            for i,batch in enumerate(train_dataloader):
+                V,atoms_info,rms,atoms_lens = (to_device(x,args.device) for x in batch)
+                out = net(V, atoms_info, atoms_lens)
+                loss = loss_fn(out, rms)
+                val_loss+=loss.item()
+        print(f'epcho:{epoch} valid loss:{val_loss/len(valid_dataset)}')
+
     if args.save_path is not None:
         torch.save(net.state_dict(), args.save_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dir', type=str, default='data/train')
+    parser.add_argument('--train_dir', type=str, default='data/train')
+    parser.add_argument('--valid_dir', type=str, default='data/val')
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--batch_size', type=int, default=4)
